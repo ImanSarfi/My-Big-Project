90800001ea060e46ea1d68b213a89969f556f986
Iman sarfi <sarfi831@gmail.com>
2024-09-14 10:14:04 -0700

    satart My-Big-Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 function App() {
   return (
     <div>
-
       <h1>My-Big-Project</h1>
+      <p>لطفا زبان مورد نظر خود را انتخاب کنید</p>
+      <p>Please choose your language</p>
+      <div>
+        <label htmlFor="farsi">فارسی</label><input type="radio"  id="languge1" name="language"/>
+        <label htmlFor="farsi">English</label><input type="radio"  id="language2" name="language" />
+      </div>
     </div>
   )
 }
